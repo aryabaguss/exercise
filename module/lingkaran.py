e8fc4397e039f41cfdef85dc5db6b0fc7ca396d6
aryabaguss <aryafake99@gmail.com>
2024-10-07 22:17:36 +0700

    round luas lingkaran

--- module/lingkaran.py
+++ module/lingkaran.py
@@ -5,11 +5,13 @@ from math import pi
 def luas():
     jari = float(input('Masukkan jari-jari lingkaran: '))
     hitung = pi * jari * jari
+    hitung  = round(hitung,3)
 
     return hitung
 
 def keliling():
     r = float(input('Masukkan jari-jari lingkaran: '))
     hitung = 2 * pi * r
-
+    hitung  = round(hitung,3)
+    
     return hitung
